50dd8cedfeefc3261caaf5a9213e12522233d387
Mark <xenojs@outlook.com>
2025-06-13 22:39:19 -0700

    Updates - Added email input - Added phone input - Added optional contact method with email - Added form submit button - Added prettier config file to repo - Added dark-gray text color to tailwind config

--- components/OptOutForm.tsx
+++ components/OptOutForm.tsx
@@ -1,25 +1,53 @@
-import { Switch } from '@headlessui/react'
-import { useForm } from "react-hook-form"
+import { Switch } from "@headlessui/react";
+import { useForm } from "react-hook-form";
 
 export type OptOutFormProps = {
 
-}
+};
 
-const OptOutForm = ({
+const OptOutForm = ({}: OptOutFormProps) => {
+  const inputClassNames = "border border-gray-200 mb-4 px-4 py-2 rounded w-full"
 
-}: OptOutFormProps) => {
-    return(
-        <div className='flex flex-col mx-4 shadow-lg shadow-slate-500/40 md:flex-row md:mx-6'>
-            <div className='bg-slate-100 border border-solid border-slate-200 flex-auto p-16 rounded-tl-lg w-full md:w-2/5'>
-                <h1 className='font-bold text-4xl'>
-                    Opt-Out
-                </h1>
-            </div>
-            <div className='bg-white border border-solid border-slate-100 flex-auto p-16 w-full md:w-3/5'>
-                World
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col mx-4 rounded-lg shadow-lg shadow-slate-500/40 md:flex-row md:mx-6">
+      <div className="bg-slate-100 border border-solid border-slate-200 flex-auto p-16 rounded-tl-lg rounded-tr-lg w-full md:rounded-bl-lg md:rounded-tr-none md:w-2/5">
+        <h1 className="font-bold text-4xl text-dark-gray">Opt-Out</h1>
+        <p className="text-xs text-slate-400">
+          Still need help? Email <a href="mailto:privacy@test.com" className="font-semibold text-blue-700 hover:text-blue-500">privacy@test.com</a>
+        </p>
+      </div>
+      <div className="bg-white border border-solid border-slate-100 flex-auto p-16 rounded-br-lg rounded-bl-lg w-full md:rounded-bl-none md:rounded-tr-lg md:w-3/5">
+        <h3 className="font-semibold mb-2 text-dark-gray">
+          Enter your email and/or phone
+        </h3>
+        <form>
+          <input
+            id="email"
+            className={inputClassNames}
+            name="email"
+            maxLength={256}
+            placeholder="Email"
+            title="Enter email address"
+            type="email"
+          />
+          <input
+            id="phone"
+            className={inputClassNames}
+            name="phone"
+            pattern="[0-9]+"
+            placeholder="Phone"
+            title="Enter phone number"
+            type="text"
+          />
+          <div className="flex justify-center items-center">
+            <button className="bg-blue-700 font-semibold px-5 py-3 rounded-full text-base text-center text-white w-full hover:bg-blue-500 md:w-1/2">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default OptOutForm
+export default OptOutForm;
