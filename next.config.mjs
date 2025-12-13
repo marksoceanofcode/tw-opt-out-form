77daab764435c2097982e16a9979aec76213231b
Mark <xenojs@outlook.com>
2025-12-13 09:05:33 -0800

    Update static export folder to build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', //Tells Next.js to generate a static site (HTML/CSS/JS files)
-  distDir: 'out', //The folder to output static files too
+  distDir: 'build', //The folder to output static files too
 };
 
 export default nextConfig;
