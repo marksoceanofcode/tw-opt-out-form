4b8d3dc253ce3f23980d2058295e3751ea932ab0
Mark <xenojs@outlook.com>
2025-12-13 09:02:20 -0800

    Updates - Packages updates - Configured setup for a static export of files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', //Tells Next.js to generate a static site (HTML/CSS/JS files)
+  distDir: 'out', //The folder to output static files too
 };
 
 export default nextConfig;
